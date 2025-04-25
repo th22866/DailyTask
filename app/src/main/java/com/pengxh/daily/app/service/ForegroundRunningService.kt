4245c1ed00549f9890b9054268cfc8680be7dc74
pengxianhong <290677893@qq.com>
2025-04-25 17:14:55 +0800

    refactor(service): 优化 ForegroundRunningService
    - 移除了不必要的 Handler 和通知更新逻辑
    - 使用 lazy delegation 优化 NotificationManager 实例化
    - 重构了通知构建过程，提高了代码可读性和性能
    - 移除了冗余的变量声明和未使用的代码

--- app/src/main/java/com/pengxh/daily/app/service/ForegroundRunningService.kt
+++ app/src/main/java/com/pengxh/daily/app/service/ForegroundRunningService.kt
@@ -4,9 +4,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
-import android.os.Handler
 import android.os.IBinder
-import android.os.Looper
 import androidx.core.app.NotificationCompat
 import com.pengxh.daily.app.R
 import com.pengxh.daily.app.utils.Constant
@@ -17,60 +15,39 @@ import com.pengxh.daily.app.utils.Constant
 class ForegroundRunningService : Service() {
 
     private val notificationId = 1
-    private val updateHandler = Handler(Looper.getMainLooper())
-    private var notificationManager: NotificationManager? = null
-    private var notificationBuilder: NotificationCompat.Builder? = null
-    private var runningTime = 0L
+    private val notificationManager by lazy { getSystemService(NOTIFICATION_SERVICE) as NotificationManager }
 
     override fun onCreate() {
         super.onCreate()
-        notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         val name = "${resources.getString(R.string.app_name)}前台服务"
         val channel = NotificationChannel(
             "foreground_running_service_channel", name, NotificationManager.IMPORTANCE_HIGH
         )
         channel.description = "Channel for Foreground Running Service"
-        notificationManager?.createNotificationChannel(channel)
-        notificationBuilder = NotificationCompat.Builder(this, "foreground_running_service_channel")
-            .setSmallIcon(R.mipmap.ic_launcher)
-            .setContentTitle("已运行0小时0分钟")
-            .setContentText(Constant.FOREGROUND_RUNNING_SERVICE_TITLE)
-            .setPriority(NotificationCompat.PRIORITY_HIGH) // 设置通知优先级
-            .setOngoing(true)
-            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-
-        val notification = notificationBuilder?.build()
-        startForeground(notificationId, notification)
+        notificationManager.createNotificationChannel(channel)
+        NotificationCompat.Builder(this, "foreground_running_service_channel").run {
+            setSmallIcon(R.mipmap.ic_launcher)
+            setContentText(Constant.FOREGROUND_RUNNING_SERVICE_TITLE)
+            setPriority(NotificationCompat.PRIORITY_HIGH) // 设置通知优先级
+            setOngoing(true)
+            setOnlyAlertOnce(true)
+            setSilent(true)
+            setCategory(NotificationCompat.CATEGORY_SERVICE)
+            setShowWhen(true)
+            setSound(null) // 禁用声音
+            setVibrate(null) // 禁用振动
+            build()
+        }.also {
+            startForeground(notificationId, it)
+        }
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        //记录通知被创建的时间
-        runningTime = System.currentTimeMillis()
-        val updateRunnable = object : Runnable {
-            override fun run() {
-                updateNotification()
-                updateHandler.postDelayed(this, 1000L * 60)
-            }
-        }
-        updateHandler.post(updateRunnable)
         return START_STICKY
-    }
-
-    private fun updateNotification() {
-        // 计算运行时长
-        val elapsedTime = System.currentTimeMillis() - runningTime
-        val hours = (elapsedTime / (1000 * 60 * 60)).toInt()
-        val minutes = (elapsedTime % (1000 * 60 * 60) / (1000 * 60)).toInt()
-        val notification = notificationBuilder?.let {
-            it.setContentTitle("已运行${hours}小时${minutes}分钟")
-            it.build()
-        }
-        notificationManager?.notify(notificationId, notification)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        updateHandler.removeCallbacksAndMessages(null)
         stopForeground(STOP_FOREGROUND_REMOVE)
     }
 
