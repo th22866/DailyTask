26857a80f5ae733dfe3a0978690ed71a834feb1b
pengxianhong <290677893@qq.com>
2025-04-25 16:34:49 +0800

    refactor(app): 优化倒计时工具类 CountDownTimerKit- 采用 by lazy 方式初始化 Handler，提高性能 -调整日志输出内容，使其更加通用 - 优化代码结构，提高可读性和可维护性

--- app/src/main/java/com/pengxh/daily/app/utils/CountDownTimerKit.kt
+++ app/src/main/java/com/pengxh/daily/app/utils/CountDownTimerKit.kt
@@ -8,8 +8,17 @@ class CountDownTimerKit(
     private val secondsInFuture: Int, private val callback: OnTimeCountDownCallback
 ) {
     private val kTag = "CountDownTimerKit"
-    private val handler = Handler(Looper.getMainLooper())
+    private val handler by lazy { Handler(Looper.getMainLooper()) }
     private var isTimerRunning = false
+
+    fun start() {
+        if (isTimerRunning) {
+            cancel()
+        }
+        handler.post(runnable)
+        isTimerRunning = true
+        Log.d(kTag, "start: 倒计时任务开始")
+    }
 
     private val runnable = object : Runnable {
         var remainingSeconds = secondsInFuture
@@ -21,23 +30,14 @@ class CountDownTimerKit(
             } else {
                 callback.onFinish()
                 isTimerRunning = false
-                Log.d(kTag, "run: 单个任务倒计时结束")
+                Log.d(kTag, "run: 倒计时任务结束")
             }
         }
-    }
-
-    fun start() {
-        if (isTimerRunning) {
-            cancel()
-        }
-        handler.post(runnable)
-        isTimerRunning = true
-        Log.d(kTag, "start: 单个任务倒计时开始")
     }
 
     fun cancel() {
         handler.removeCallbacks(runnable)
         isTimerRunning = false
-        Log.d(kTag, "cancel: 取消单个任务倒计时")
+        Log.d(kTag, "cancel: 倒计时任务取消")
     }
 }
